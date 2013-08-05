5293106b9dcd6e3183a138795c43978806b7dd8a
RudeDude <rudedude@gmail.com>
2013-08-05 11:37:12 -0400

    Re-committing my version as tip of master.

--- LeapScroll.js
+++ LeapScroll.js
@@ -1,1 +1,28 @@
-javascript:(function(){function runScript(){var e={};var t={};var n=document.body;Leap.loop(function(t){var r={};var i={};for(var s=0,o=t.pointables.length;s!=o;s++){var u=t.pointables[s];var a=e[u.id];var f=n.scrollTop;if(u.tipPosition[1]-250>0){n.scrollTop=f-=10}else{n.scrollTop=f+=10}}})}if(typeof Leap=="undefined"){var jsCode=document.createElement("script");jsCode.setAttribute("src","https://reindeer.arvixe.com/~suiyoken/leapchrome/leap.min.js");jsCode.onload=runScript;document.body.appendChild(jsCode)}else{runScript()}}());
+javascript:(function(){
+function runScript(){
+        var e={};
+        var t={};
+        var n=document.body;
+        Leap.loop(function(t){
+                var r={};
+                var i={};
+                for(var s=0, o=t.pointables.length; s!=o; s++){
+                        var u=t.pointables[s];
+                        var a=e[u.id];
+                        var f=n.scrollTop;
+                        if(document.hasFocus()){
+                                if(u.tipPosition[1]-325>0){n.scrollTop=f-=150}
+                                if(u.tipPosition[1]-125>0){n.scrollTop=f-=5}
+                                if(u.tipPosition[1]-90<0){n.scrollTop=f+=5}
+                        }
+                }
+        })
+}
+if(typeof Leap=="undefined"){
+        var jsCode=document.createElement("script");
+        jsCode.setAttribute("src","https://js.leapmotion.com/0.2.0/leap.min.js");
+        jsCode.onload=runScript;document.body.appendChild(jsCode)
+}else{
+        runScript()
+}
+}());
